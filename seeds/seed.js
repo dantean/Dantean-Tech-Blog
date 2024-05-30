1774cd72e4167f2de3cee11183650fefee75d6d1
Joseph Monticelli <jdmonticelli@gmail.com>
2024-05-29 21:37:34 -0400

    updated models, seeds, and login functions

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Post,Comment } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,9 +13,26 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
+  for (const post of postData) {
+    await Post.create({
+      ...post,
+      
+      //  Math.random() - 0 to 1 with decimal  0*3 to 1 *3 =>0 to 3
+      // Math.floor will get rid of decimal, it rounds down
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+  
+  const posts= await Post.findAll();
+
+  
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+      
+      //  Math.random() - 0 to 1 with decimal  0*3 to 1 *3 =>0 to 3
+      // Math.floor will get rid of decimal, it rounds down
+      post_id: posts[Math.floor(Math.random() * posts.length)].id,
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
